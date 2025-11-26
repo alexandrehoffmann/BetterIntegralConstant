2533e2bbb7f08b9256a0a7b35f9a8f52df0f5567
Alexandre Hoffmann <alexandre.hoffmann.etu@gmail.com>
2025-11-26 16:02:42 +0100

    Add foreach overload with step parameter

--- include/BIC/Loops.hpp
+++ include/BIC/Loops.hpp
@@ -15,7 +15,9 @@ constexpr UnaryFunc&& foreach(FixedArray<T, VALUES...>, UnaryFunc&& func) { (fun
 	
 template<typename Size, Size FIRST, Size BOUND, typename UnaryFunc> 
 constexpr UnaryFunc&& foreach(Fixed<Size, FIRST>, Fixed<Size, BOUND>, UnaryFunc&& func) { return foreach(seq<Size, FIRST, BOUND>, std::forward<UnaryFunc>(func)); }
-	
+
+template<typename Size, Size FIRST, Size BOUND, Size STEP, typename UnaryFunc> 
+constexpr UnaryFunc&& foreach(Fixed<Size, FIRST>, Fixed<Size, BOUND>, Fixed<Size, STEP>, UnaryFunc&& func) { return foreach(seq<Size, FIRST, BOUND, STEP>, std::forward<UnaryFunc>(func)); }
 	
 } // namespace BIC
 
