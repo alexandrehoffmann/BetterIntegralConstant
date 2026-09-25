ee2feb8d46234d484e049dd723129b06bbe88e57
Alexandre Hoffmann <alexandre.hoffmann.etu@gmail.com>
2026-09-25 19:43:50 +0200

    -

---
+++ include/BIC/misc/Is_ComparableTo.hpp
@@ -0,0 +1,44 @@
+#ifndef BIC_MISC_IS_COMPARABLE_TO_HPP
+#define BIC_MISC_IS_COMPARABLE_TO_HPP
+
+#include <concepts>
+
+namespace BIC
+{
+
+namespace misc
+{
+/**
+ * @brief Concept ensuring that two types are mutually comparable.
+ *
+ * @tparam Lhs Left-hand-side type.
+ * @tparam Rhs Right-hand-side type.
+ *
+ * This concept is satisfied if and only if both types support all six
+ * comparison operators, and the result of each comparison is convertible
+ * to `bool`.
+ *
+ * Example:
+ * @code
+ * template<typename T, typename U>
+ * requires BIC::misc::Is_ComparableTo<T, U>
+ * bool are_equal(const T& a, const U& b) {
+ *     return a == b;
+ * }
+ * @endcode
+ */
+template <typename Lhs, typename Rhs>
+concept Is_ComparableTo = requires(Lhs a, Rhs b) {
+  { a == b } -> std::convertible_to<bool>;
+  { a != b } -> std::convertible_to<bool>;
+  { a < b } -> std::convertible_to<bool>;
+  { a <= b } -> std::convertible_to<bool>;
+  { a > b } -> std::convertible_to<bool>;
+  { a >= b } -> std::convertible_to<bool>;
+};
+
+} // namespace misc
+
+} // namespace ic
+
+#endif // BIC_MISC_IS_COMPARABLE_TO_HPP
