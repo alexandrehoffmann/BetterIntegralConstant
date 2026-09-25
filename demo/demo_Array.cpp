0494e3d1134b547bcfb2379441f1856b675ba1f4
Alexandre Hoffmann <alexandre.hoffmann.etu@gmail.com>
2026-09-25 08:04:54 +0200

    format

--- demo/demo_Array.cpp
+++ demo/demo_Array.cpp
@@ -7,9 +7,9 @@ int main()
   std::println("Printing a sequence : {}", BIC::seq<size_t, 0, 10, 3>);
 
   std::println("Iterating over a range from Fixed<int,0> to Fixed<int,9>");
-  BIC::foreach(BIC::fixed<int, 0>, BIC::fixed<int, 10>, [](const auto i) { std::print("i = {} is i fixed ? {}", i, isFixed(i)); });
+  BIC::foreach(BIC::fixed<int, 0>, BIC::fixed<int, 10>, [](const auto i) { std::println("  * i = {} is i fixed ? {}", i, isFixed(i)); });
   std::println("Iterating over a reversed range from Fixed<int,9> to Fixed<int,0>");
-  BIC::foreach(BIC::fixed<int, 9>, BIC::fixed<int, 0>, BIC::fixed<int, -1>, [](const auto i) { std::print("i = {} is i fixed ? {}", i, isFixed(i)); });
+  BIC::foreach(BIC::fixed<int, 9>, BIC::fixed<int, 0>, BIC::fixed<int, -1>, [](const auto i) { std::println("  * i = {} is i fixed ? {}", i, isFixed(i)); });
   std::println("Iterating over the elements of a FixedArray<int,2,4,3,-1,9>");
   BIC::foreach(BIC::fixedArray<int, 2, 4, 3, -1, 9>, [](const auto i) { std::print("i = {} is i fixed ? {}", i, isFixed(i)); });
   std::println("Iterating over the elements of a reversed FixedArray<int,2,4,3,-1,9>");
